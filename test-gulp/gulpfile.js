c1a1b23c54f0b8085ad994fb09507470472233ea
Jessica Kennedy <jessica.kennedy@inmar.com>
2016-06-15 08:13:16 -0500

    updated test gulp & added inmar slide

--- test-gulp/gulpfile.js
+++ test-gulp/gulpfile.js
@@ -4,6 +4,8 @@ const del  = require('del')
 const minimist  = require('minimist')
 const gutil = require('gulp-util')
 const uglify = require('gulp-uglify')
+const connect = require('gulp-connect')
+const runSequence = require('run-sequence')
 
 const options = minimist(process.argv.slice(2), {
   default: {
@@ -19,7 +21,12 @@ gulp.task('default', () => {
 })
 
 gulp.task('build', ['css:build', 'js:build', 'html:build'])
-gulp.task('watch', ['css:watch', 'js:watch', 'html:watch'])
+gulp.task('watch', function(callback){
+  return runSequence(
+    'serve',
+    ['css:watch', 'js:watch', 'html:watch']
+  )
+})
 gulp.task('clean', ['css:clean', 'js:clean', 'html:clean'])
 
 // css
@@ -27,11 +34,12 @@ gulp.task('css:build', () => {
   return gulp
     .src('app/**/*.css')
     .pipe(autoprefixer({ browsers: ['last 2 versions', '> 5%'] }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'))
+    .pipe(connect.reload());
 })
 
 gulp.task('css:watch', () => {
-  gulp.watch(`app/**/*.css`, ['build']);
+  gulp.watch(`app/**/*.css`, ['css:build']);
 })
 
 gulp.task('css:clean', () => {
@@ -43,20 +51,30 @@ gulp.task('js:build', () => {
   return gulp
     .src('app/**/*.js')
     .pipe(options.production ? uglify() : gutil.noop())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'))
+    .pipe(connect.reload());
 })
 
 gulp.task('js:watch', () => {
-  gulp.watch(`app/**/*.js`, ['build']);
+  gulp.watch(`app/**/*.js`, ['js:build']);
 })
 
 // html
 gulp.task('html:build', () => {
   return gulp
     .src('app/**/*.html')
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'))
+    .pipe(connect.reload());
 })
 
 gulp.task('html:watch', () => {
-  gulp.watch(`app/**/*.html`, ['build']);
+  gulp.watch(`app/**/*.html`, ['html:build']);
+})
+
+
+
+gulp.task('serve', () => {
+  connect.server({
+    livereload: true
+  })
 })
